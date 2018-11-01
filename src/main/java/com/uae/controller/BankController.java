05b0c2259e7bc7448be88fa1ffdce81d7261499d
madhanmanik <madhanmanik@gmail.com>
2018-11-01 18:32:40 +0530

    Erueka server1

---
+++ src/main/java/com/uae/controller/BankController.java
@@ -0,0 +1,19 @@
+package com.uae.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank")
+public class BankController {
+
+	 @GetMapping("/txn")
+	    public String getTransactionDetails(@RequestParam String xrNo) {
+	        return xrNo+"getTrn";
+	    }
+	
+	 
+	 
+}
